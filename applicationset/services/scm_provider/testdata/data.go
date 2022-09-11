8400be5dbbf29f132aeb72ab43566aa93d9e8e43
Song <Even.Song@connext.com.cn>
2022-09-11 16:18:39 +0800

    delete files

--- applicationset/services/scm_provider/testdata/data.go
+++
@@ -1,6 +0,0 @@
-package testdata
-
-import _ "embed"
-
-//go:embed repos_gitea_go-sdk_contents_gitea.json
-var ReposGiteaGoSdkContentsGiteaResponse string
--- hack/installers/install-codegen-tools.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -eux -o pipefail
-
-KUSTOMIZE_VERSION=4.4.1 "$(dirname $0)/../install.sh" kustomize protoc
